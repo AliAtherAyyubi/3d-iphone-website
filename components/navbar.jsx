a081bde9132b69bdcec99880bf35c4fa908e4c29
AliAtherAyyubi <info2atherayyubi@gmail.com>
2025-02-15 13:01:46 +0500

    Navbar code

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import { appleImg, bagImg, searchImg } from "@/utils";
-// import {navLists} from '../constants/index.js'
 import {navLists} from '../constants'
 //
 function Navbar() {
   return (
     
-    <header className="w-full p-4">
-      <nav className="flex justify-between items-center ">
+    <header className="w-full py-5 sm:px-10 px-5 flex-center">
+      <nav className="flex screen-max-width w-full ">
         <Image src={appleImg} alt="apple logo" height={18} width={18} />
 
-        <div className="flex-center flex-1 max-sm:hidden">
+        <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((navitem) => (
             <div
               key={navitem} 
@@ -22,7 +21,7 @@ function Navbar() {
           ))}
         </div>
 
-        <div className="flex items-baseline gap-4">
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <Image src={searchImg} height={18} width={18} />
           <Image src={bagImg} height={18} width={18} />
         </div>
